77f010e79b1be74f854b003fc16b9a8045b34b40
Quoc <quoc.nguyen@audiencemedia.com>
2014-09-17 23:22:03 +0700

    remove bin&gen folders

--- HoGoDev_RicohApps/gen/com/ricoh/printer/sender/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ricoh.printer.sender;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- ScannerApp/ScanAPILib/gen/jp/co/ricoh/ssdk/sample/app/scan/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package jp.co.ricoh.ssdk.sample.app.scan;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
