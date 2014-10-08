a976be3da8b1e50fb755e6c52363594aaa584266
Giang Nguyen <giangnguyen.tale@gmail.com>
2014-10-08 23:21:06 +0700

    add missing attribute

--- ScannerApp/HoGoAPI/src/com/gso/hogoapi/model/History.java
+++ ScannerApp/HoGoAPI/src/com/gso/hogoapi/model/History.java
@@ -6,8 +6,14 @@ public class History {
     public String packID;//: "25e19385971b42f28b13209b778f58cb"
     public String documentName;//: "TestFileLarge"
     public String recipientName;//: "New Recipient"
+    public String recipientEmail;
     public String createDate;//: "2014/09/18 14:19:09"
     public String point;//: 2
     public String paymentType;//: 0
     public String total;//: 269
+    public int status;//": 1,
+	public int num_of_download;//": 2,
+	public String be_download_id;//": "425381832db04ef98c9f5ca8162c756f",
+	public int opened_status;//": 1,
+	public String opened_date;//": "2014/09/18 14:27:04"
 }
